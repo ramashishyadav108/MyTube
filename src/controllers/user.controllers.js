8fc2b587be1605348d5b5c83640213ec7b6e0445
ramashishyadav108 <ray09112004@gmail.com>
2025-06-23 22:24:44 +0530

    setup configured

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,9 @@
+import  {asyncHandler} from '../utils/asyncHandler.js';
+
+const registerUser=asyncHandler(async (req,res)=>{
+    return res.status(400).json({
+        message:"register for mytube"
+    })
+})
+
+export {registerUser};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
-
+import {app }   from './app.js'
 import express from 'express'
 
 
@@ -11,7 +11,7 @@ dotenv.config({
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT||8000,()=>{
-        console.log(`Server is running of port: ${process.env.PORT}`)
+        console.log(`Server is running at port: ${process.env.PORT}`)
     })
 })
 .catch((err)=>{
