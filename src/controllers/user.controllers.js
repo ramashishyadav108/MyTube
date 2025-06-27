ee66f95cc7f21e32fa4dd64dbf0f94a87d626bab
ramashishyadav108 <ray09112004@gmail.com>
2025-06-27 22:12:45 +0530

    Add getUserChannelProfile function

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -264,7 +264,7 @@ const changeCurrentPassword= asyncHandler(async(req,res)=>{
 const getCurrentUser=asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json( 200,req.user,'Current user fetched successfully')
+    .json( new ApiResponse(200,req.user,'Current user fetched successfully'))
 })
 
 const updateAccountDetails=asyncHandler(async(req,res)=>{
@@ -274,7 +274,7 @@ const updateAccountDetails=asyncHandler(async(req,res)=>{
         throw new ApiError(400,'Full name and email are required')
     }
 
-   const user= User.findByIdAndUpdate(
+   const user=await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set:{
@@ -354,6 +354,83 @@ const updateUserCoverImage=asyncHandler(async(req,res)=>{
 
 })
 
+
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+
+    const {username}=req.params;
+    if(!username?.trim())
+    {
+        throw new ApiError(400,'Username is required')
+    }
+
+    const channel= await User.findOne([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:'subscriptions',
+                localField:'_id',
+                foreignField:'channel',
+                as:'subscribers'
+            }
+        },
+        {
+            $lookup:{
+                from:'subscriptions',
+                localField:'_id',
+                foreignField:'subscriber',
+                as:'subscribedTo'
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount: {
+                    $size:'$subscribers'
+                },
+                channelsSubscribedToCount:{
+                    $size:'$subscribedTo'
+                },
+                isSybscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,'$subscribers.subscriber']},
+                        then:true,
+                        else:false
+                    }
+                }
+
+            } 
+            
+        },
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSybscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404,'Channel not found')
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,channel[0],'Channel profile fetched successfully')
+    )
+
+})
+
+
 export {registerUser,
     loginUser,
     logoutUser,
@@ -362,5 +439,6 @@ export {registerUser,
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
     }; 
