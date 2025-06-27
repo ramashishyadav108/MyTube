d508690036ebfd124398b8da843d6fa05509e974
ramashishyadav108 <ray09112004@gmail.com>
2025-06-27 23:56:25 +0530

    Fix route for getting user channel profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,13 @@
 import {Router}  from 'express'
-import {registerUser} from '../controllers/user.controllers.js'
+import {changeCurrentPassword,
+     getWatchHistory, 
+     registerUser, 
+     updateAccountDetails, 
+     updateUserAvatar, 
+     updateUserCoverImage,
+     getCurrentUser,
+     getUserChannelProfile
+     } from '../controllers/user.controllers.js'
 import {upload} from '../middlewares/multer.middleware.js'
 import {verifyJWT} from '../middlewares/auth.middleware.js'
 import {loginUser,logoutUser} from '../controllers/user.controllers.js'
@@ -30,6 +38,23 @@ router.route('/login').post(loginUser)
 //secured routes
 router.route('/logout').post(verifyJWT,logoutUser)
 router.route('/refresh-token').post(refreshAccessToken)
+
+
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+
+router.route('/current-user').get(verifyJWT,getCurrentUser)
+
+router.route('/update-account').patch(verifyJWT,updateAccountDetails)
+
+router.route('/avatar').patch(verifyJWT,upload.single('avatar'),updateUserAvatar)
+
+router.route('/cover-image').patch(verifyJWT.upload.single('/coverImage'),updateUserCoverImage)
+
+router.route('/c/:username').get(verifyJWT.getUserChannelProfile)
+
+
+router.route('/history').get(verifyJWT,getWatchHistory)
+
 
 
 
